1a0fb029e54c29c35157130049b11bd58b34f762
czaree <lbov.ct@gmail.com>
2021-07-23 12:29:16 -0400

    Green Living Content Addition
    Hopefully this is my final commit. I added and linked together all pages of information about green living, added the questions and answers to the quiz submitted before,  and created an accordion menu below my topic to store all of the subpages.

--- greenliving_quiz_validation.js
+++ greenliving_quiz_validation.js
@@ -1,4 +1,4 @@
-//tutorial followed at https://www.codemahal.com/video/javascript-quiz-tutorial/
+//tutorial followed at https://www.codemahal.com/video/javascript-quiz-tutorial/ - slightly modified to suit individual needs
 //initial variables
 var pos = 0;
 var correct = 0;
@@ -7,21 +7,45 @@ var quiz, quiz_status, question, chosen_answer, possible_answers, ans_A, ans_B, 
 //question array
 var questions = [
   {
-    question: "Question 1 Placeholder.",
-    a: "1",
-    b: "2",
-    c: "3",
-    d: "4",
-    answer: "C"
+    question: "Which of the following is the most eco-friendly mode of transportation?",
+    a: "Flying a Plane",
+    b: "Walking",
+    c: "Taking the bus",
+    d: "Driving a car",
+    answer: "B"
     },
   {
-    question: "Question 2 Placeholder.",
-    a: "2",
-    b: "3",
-    c: "4",
-    d: "5",
-    answer: "C"
+    question: "If you find yourself with way more food than you could ever eat, what environmentally safe actions can you take to get rid of it?",
+    a: "Throw everything away",
+    b: "Put it back into the pantry until everything is expired",
+    c: "Flush it down the toilet",
+    d: "Donate as much as possible to a local shelter or food pantry, and offer the rest to people you know.",
+    answer: "D"
   },
+  {
+    question: "In the summer, what is an effective way to reduce electricity use?",
+    a: "Leaving the air conditioning on 24 hours a day",
+    b: "Opening all windows with screens for a short amount of time to let air circulate",
+    c: "Plugging 5 different fans into the wall",
+    d: "Taking a cold shower, and then using a blow drier",
+    answer: "C",
+    },
+    {
+      question: "Which of the following activities is it safe to use greywater to do?",
+      a: "Use it as drinking water",
+      b: "Water a yard or garden",
+      c: "Take a bath or shower",
+      d: "Doing the dishes",
+      answer: "C",
+      },
+      {
+        question: "What is the highest contributor to greenhouse gas emissions, making up about 29% of all produced?",
+        a: "Travel and transport",
+        b: "Industry and manufacturing",
+        c: "Agriculture",
+        d: "Generating Electricity",
+        answer: "C",
+      },
 ];
 
 //shortening down code
@@ -34,8 +58,8 @@ function displayQuestion() {
   quiz = get("quiz");
 //result display and reset
   if (pos >= questions.length) {
-    quiz.innerHTML = "<center><h3>You got "+correct+" out of "+questions.length+" questions correct. Congrats!</h3></center>";
-    get("quiz_status").innerHTML = "<center>Quiz complete</center>";
+    quiz.innerHTML = "<h3>You got "+correct+" out of "+questions.length+" questions correct. Congrats!</h3>";
+    get("quiz_status").innerHTML = "Quiz complete";
     pos = 0;
     correct = 0;
     return false;
